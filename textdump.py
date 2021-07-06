0025843854dc4ae81b725d2ce4e17cdd4e2fedfe
blackstream-x <rainer@blackstream.de>
2021-07-06 12:41:52 +0200

    Add C0 and C1 controls names taken from https://unicode.org/charts/PDF/U0000.pdf and https://unicode.org/charts/PDF/U0080.pdf

--- textdump.py
+++ textdump.py
@@ -20,6 +20,71 @@ CARRIAGE_RETURN = '\r'
 HORIZONTAL_TAB = '\t'
 LINE_FEED = '\n'
 
+CONTROLS_NAMES = {
+    '\u0000': 'NULL',
+    '\u0001': 'START OF HEADING',
+    '\u0002': 'START OF TEXT',
+    '\u0003': 'END OF TEXT',
+    '\u0004': 'END OF TRANSMISSION',
+    '\u0005': 'ENQUIRY',
+    '\u0006': 'ACKNOWLEDGE',
+    '\u0007': 'BELL',
+    '\u0008': 'BACKSPACE',
+    '\u0009': 'CHARACTER TABULATION',
+    '\u000A': 'LINE FEED (LF)',
+    '\u000B': 'LINE TABULATION= vertical tabulation (VT)',
+    '\u000C': 'FORM FEED (FF)',
+    '\u000D': 'CARRIAGE RETURN (CR)',
+    '\u000E': 'SHIFT OUT',
+    '\u000F': 'SHIFT IN',
+    '\u0010': 'DATA LINK ESCAPE',
+    '\u0011': 'DEVICE CONTROL ONE',
+    '\u0012': 'DEVICE CONTROL TWO',
+    '\u0013': 'DEVICE CONTROL THREE',
+    '\u0014': 'DEVICE CONTROL FOUR',
+    '\u0015': 'NEGATIVE ACKNOWLEDGE',
+    '\u0016': 'SYNCHRONOUS IDLE',
+    '\u0017': 'END OF TRANSMISSION BLOCK',
+    '\u0018': 'CANCEL',
+    '\u0019': 'END OF MEDIUM',
+    '\u001A': 'SUBSTITUTE',
+    '\u001B': 'ESCAPE',
+    '\u001C': 'INFORMATION SEPARATOR FOUR (FS)',
+    '\u001D': 'INFORMATION SEPARATOR THREE (GS)',
+    '\u001E': 'INFORMATION SEPARATOR TWO (RS)',
+    '\u001F': 'INFORMATION SEPARATOR ONE (US)',
+    '\u007f': 'DELETE',
+    '\u0082': 'BREAK PERMITTED HERE',
+    '\u0083': 'NO BREAK HERE',
+    '\u0084': '• formerly known as INDEX',
+    '\u0085': 'NEXT LINE (NEL)',
+    '\u0086': 'START OF SELECTED AREA',
+    '\u0087': 'END OF SELECTED AREA',
+    '\u0088': 'CHARACTER TABULATION SET',
+    '\u0089': 'CHARACTER TABULATION WITHJUSTIFICATION',
+    '\u008A': 'LINE TABULATION SET',
+    '\u008B': 'PARTIAL LINE FORWARD',
+    '\u008C': 'PARTIAL LINE BACKWARD',
+    '\u008D': 'REVERSE LINE FEED',
+    '\u008E': 'SINGLE SHIFT TWO',
+    '\u008F': 'SINGLE SHIFT THREE',
+    '\u0090': 'DEVICE CONTROL STRING',
+    '\u0091': 'PRIVATE USE ONE',
+    '\u0092': 'PRIVATE USE TWO',
+    '\u0093': 'SET TRANSMIT STATE',
+    '\u0094': 'CANCEL CHARACTER',
+    '\u0095': 'MESSAGE WAITING',
+    '\u0096': 'START OF GUARDED AREA',
+    '\u0097': 'END OF GUARDED AREA',
+    '\u0098': 'START OF STRING',
+    '\u009A': 'SINGLE CHARACTER INTRODUCER',
+    '\u009B': 'CONTROL SEQUENCE INTRODUCER',
+    '\u009C': 'STRING TERMINATOR',
+    '\u009D': 'OPERATING SYSTEM COMMAND',
+    '\u009E': 'PRIVACY MESSAGE',
+    '\u009F': 'APPLICATION PROGRAM COMMAND',
+}
+
 ESCAPES = {
     CARRIAGE_RETURN: r'\r',
     HORIZONTAL_TAB: r'\t',
@@ -97,7 +162,12 @@ class TextDumper:
             try:
                 unicode_name = unicodedata.name(character)
             except ValueError:
-                unicode_name = '–'
+                try:
+                    unicode_name = CONTROLS_NAMES[character]
+                except KeyError:
+                    unicode_name = 'unnamed unicode character %s' % (
+                        codepoint_representation(codepoint))
+                #
             #
             yield (self.representation(character), codepoint, unicode_name)
         #
